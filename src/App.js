a7ddf818eabc0ea02d6c65397ead4874b111694b
Pornchanok <pornchanok.s@kerrylogistics.com>
2019-07-26 14:05:45 +0700

    install bootsrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// same require's node
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function component
 function App() {
   return (
     <div className="App">
